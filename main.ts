1cc5b5ac9df843a90084d78bdad5b02ae6d2bc00
Staytuned-GH <162489454+staytuned-gh@users.noreply.github.com>
2024-12-10 14:45:13 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 const KV = await Deno.openKv();
 Deno.serve(() => {
-    KV.set([Date.now().toString()],"hello");
+    KV.set([Date.now().toString(),1],"hello");
     return new Response("Sample2!")
 })
